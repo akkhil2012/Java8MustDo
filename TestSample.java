4344a33015c92b95c95c0d6ad37e8e2a4768109f
AKHIL GUPTA <akkhil2012@gmail.com>
2024-01-13 08:13:59 +0530

    Basic Stream Splitter in java8

---
+++ TestSample.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class TestSample {
+
+    public static void main(String[] args) {
+
+
+        //case1: Basic native splitter
+        List<String> lst = new ArrayList<>();
+        lst.add("akkhil");
+        lst.add("kumar");
+        lst.add("gupta");
+        Spliterator<String> spliterator = lst.spliterator();
+        Stream<String>  stream = StreamSupport.stream(spliterator,false);
+        stream.map(String::toUpperCase)
+                .forEach(System.out::println);
+
+
+    }
+
+
+
+}
